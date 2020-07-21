66bd87d2ace005a583760296acf4c0e61ad74a25
Patryk Płaczek <patryk.placzek@wp.pl>
2020-07-21 11:38:43 +0200

    Rebrand site v2 & add feature area

--- src/js/script.js
+++ src/js/script.js
@@ -5,45 +5,11 @@
     if (prevScrollpos > currentScrollPos) {
       document.getElementById("navbar").style.top = "0";
     } else {
-      document.getElementById("navbar").style.top = "-106px";
+      document.getElementById("navbar").style.top = "-116px";
     }
     prevScrollpos = currentScrollPos;
   }
 }(jQuery));
-
-(function ($){
-  $.fn.counter = function() {
-    const $this = $(this),
-    numberFrom = parseInt($this.attr('data-from')),
-    numberTo = parseInt($this.attr('data-to')),
-    delta = numberTo - numberFrom,
-    deltaPositive = delta > 0 ? 1 : 0,
-    time = parseInt($this.attr('data-time')),
-    changeTime = 10;
-
-    let currentNumber = numberFrom,
-    value = delta*changeTime/time;
-    var interval1;
-    const changeNumber = () => {
-      currentNumber += value;
-      (deltaPositive && currentNumber >= numberTo) || (!deltaPositive &&currentNumber<= numberTo) ? currentNumber=numberTo : currentNumber;
-      this.text(parseInt(currentNumber));
-      currentNumber == numberTo ? clearInterval(interval1) : currentNumber;
-    }
-
-    interval1 = setInterval(changeNumber,changeTime);
-  }
-}(jQuery));
-
-$(document).ready(function(){
-
-  $('.count1').counter();
-  $('.count2').counter();
-  $('.count3').counter();
-  $('.count4').counter();
-
-});
-
 
 $(function() {
   $('.intro').addClass('go');
