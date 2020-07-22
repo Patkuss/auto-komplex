41b7ece9d5f64a0789e3a70f6b60b9fc0dee30ba
Patryk Płaczek <patryk.placzek@wp.pl>
2020-07-22 14:10:37 +0200

    Add gallery area

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,6 @@
 (function ($){
+
+  // navbar //
   var prevScrollpos = window.pageYOffset;
   window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
@@ -11,6 +13,7 @@
   }
 }(jQuery));
 
+// animated text //
 $(function() {
   $('.intro').addClass('go');
 
@@ -22,3 +25,55 @@ $(function() {
 
   });
 })
+
+// gallery //
+
+let modalId = $('#image-gallery');
+
+$(document)
+  .ready(function () {
+
+    loadGallery(true, 'a.thumbnail');
+
+    function loadGallery(setIDs, setClickAttr) {
+      let current_image,
+        selector,
+        counter = 0;
+
+      $('#show-next-image, #show-previous-image')
+        .click(function () {
+          if ($(this)
+            .attr('id') === 'show-previous-image') {
+            current_image--;
+          } else {
+            current_image++;
+          }
+
+          selector = $('[data-image-id="' + current_image + '"]');
+          updateGallery(selector);
+        });
+
+      function updateGallery(selector) {
+        let $sel = selector;
+        current_image = $sel.data('image-id');
+        $('#image-gallery-title')
+          .text($sel.data('title'));
+        $('#image-gallery-image')
+          .attr('src', $sel.data('image'));
+        disableButtons(counter, $sel.data('image-id'));
+      }
+
+      if (setIDs == true) {
+        $('[data-image-id]')
+          .each(function () {
+            counter++;
+            $(this)
+              .attr('data-image-id', counter);
+          });
+      }
+      $(setClickAttr)
+        .on('click', function () {
+          updateGallery($(this));
+        });
+    }
+  });
