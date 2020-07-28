dbd16fc98edf4d61d6c44ab3545aa907dd909b6d
Patryk Płaczek <patryk.placzek@wp.pl>
2020-07-28 16:14:11 +0200

    Fix hide/show script for mobile

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,19 @@
-(function ($){
-
   // navbar //
+
+(function ($){
   var prevScrollpos = window.pageYOffset;
   window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-      document.getElementById("navbar").style.top = "0";
+    if (window.outerWidth > 991) {
+      if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar").style.top = "0";
+      } else {
+        document.getElementById("navbar").style.top = "-116px";
+      }
+      prevScrollpos = currentScrollPos;
     } else {
-      document.getElementById("navbar").style.top = "-116px";
+      prevScrollpos = currentScrollPos;
     }
-    prevScrollpos = currentScrollPos;
   }
 }(jQuery));
 
